4a6c44fe5e206d67b1a589ceb6b63e66f958994c
Rajat1811 <rajatbains73@gmail.com>
2022-08-26 03:51:29 +0530

    final

--- Project/contact_book/tests.py
+++ Project/contact_book/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.urls import include, path, reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        url = reverse("/contact")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
